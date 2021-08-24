c98cfc09603968a64f641ea619d0ef6d6c6e3931
Bertrand Marron <bertrand@bizon.solutions>
2021-08-24 11:44:03 +0200

    feat: initial release (#1)

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+
+    '@semantic-release/release-notes-generator',
+
+    '@semantic-release/github',
+
+    '@semantic-release/npm',
+
+    [
+      '@semantic-release/git',
+      {
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.gitTag}',
+      },
+    ],
+  ],
+}
